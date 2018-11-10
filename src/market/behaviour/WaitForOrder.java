084ccd5b03b427ab61c0a63ff980f3bcfa8df9a5
Almeida-Oco <intjalmeida@gmail.com>
2018-11-10 00:52:43 +0000

    Mid-commit

--- src/market/behaviour/WaitForOrder.java
+++ src/market/behaviour/WaitForOrder.java
@@ -20,13 +20,21 @@ public class WaitForOrder extends Behaviour {
   public void action() {
     ACLMessage msg = this.agent.blockingReceive();
     if (msg != null) {
-      System.out.println("Got msg: '" + msg.getContent() + "'");
-      // Do something with message
+      String reply_with = msg.getReplyWith();
+      if (reply_with != null) {
+        ACLMessage reply = new ACLMessage(ACLMessage.CONFIRM);
+        reply.setInReplyTo(reply_with);
+        reply.addReceiver(msg.getSender());
+        this.agent.send(reply);
+        System.out.println("    Sent reply");
+      }
+      else {
+        System.out.println("Got msg: '" + msg.getContent() + "'");
+      }
     }
   }
 
   public boolean done() {
     return false;
   }
-
 }
--- src/shareholder/ShareholderAgent.java
+++ src/shareholder/ShareholderAgent.java
@@ -3,6 +3,7 @@ package shareholder;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.domain.AMSService;
+import jade.lang.acl.ACLMessage;
 import jade.core.behaviours.Behaviour;
 import jade.domain.FIPAAgentManagement.SearchConstraints;
 import jade.domain.FIPAAgentManagement.AMSAgentDescription;
@@ -14,14 +15,12 @@ public class ShareholderAgent extends Agent {
   private AID companies;
 
   public void setup() {
-    System.out.println("Hello World!");
-    AMSAgentDescription market_desc = this.searchForAgent(MARKET_NAME);
-    if (market_desc == null) {
-      System.exit(1);
-    }
-
-    this.market = market_desc.getName();
-
+    this.market = new AID(MARKET_NAME, false);
+    ACLMessage msg = new ACLMessage(0);
+    msg.setContent("Hello from agent '" + this.getLocalName() + "'!");
+    msg.addReceiver(this.market);
+    msg.setSender(this.getAID());
+    this.send(msg);
     // this.addBehaviour(new WorkingBehaviour());
   }
 
@@ -29,19 +28,21 @@ public class ShareholderAgent extends Agent {
     AID target = new AID(name, false);
     try {
       SearchConstraints c = new SearchConstraints();
-      c.setMaxResults(Long.valueOf(1));
+      c.setMaxResults(Long.valueOf(-1));
+      System.out.println("Starting search...");
       AMSAgentDescription[] agents = AMSService.search(this, target, new AMSAgentDescription(), c);
+      System.out.println("Finished search");
       if (agents.length >= 1 && agents[0] != null) {
         return agents[0];
       }
       else {
-        System.err.println("Failed to get agent '" + name + "', maybe not started?");
+        System.err.println("Failed to get agent '" + name + "', maybe not started? '" + agents + "'");
         return null;
       }
     }
 
     catch (Exception e) {
-      System.err.println("Failed to get agent '" + name + "', maybe not started?\n - " + e.getMessage());
+      System.err.println("Failed to get agent '" + name + "', maybe not started?\n - " + e.toString());
       return null;
     }
   }
